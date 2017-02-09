f18e148b937aadd008e652bace34abb005bc930e
Ashutosh <ashutoshsolanki@yahoo.co.in>
2017-02-09 09:55:01 +0530

    Client side angular app framework

--- app_client/app.js
+++ app_client/app.js
@@ -2,18 +2,24 @@
 
   angular.module('handbook', ['ngRoute']);
 
-  function config ($routeProvider) {
+  function config ($routeProvider,$locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'home/home.view.html',
         controller: 'homeCtrl',
         controllerAs: 'vm'
       })
+      .when('/about', {
+        templateUrl: '/common/views/genericText.view.html',
+        controller: 'aboutCtrl',
+        controllerAs: 'vm'
+      })
       .otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
   }
 
   angular
     .module('handbook')
-    .config(['$routeProvider', config]);
+    .config(['$routeProvider', '$locationProvider',config]);
 
 })();
