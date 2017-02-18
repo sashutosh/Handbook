412975033d29e2f9fd00d66613ccc57d3ae65d4f
Ashutosh <ashutoshsolanki@yahoo.co.in>
2017-02-18 06:28:32 +0530

    Register new user pagw working

--- app_client/app.js
+++ app_client/app.js
@@ -14,6 +14,16 @@
         controller: 'aboutCtrl',
         controllerAs: 'vm'
       })
+      .when('/register', {
+        templateUrl: '/auth/register/register.view.html',
+        controller: 'registerCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/login', {
+        templateUrl: '/auth/login/login.view.html',
+        controller: 'loginCtrl',
+        controllerAs: 'vm'
+      })
       .otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }
