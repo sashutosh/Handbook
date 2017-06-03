8ddd729d457ec895677f3a5bca89355b0f7e1f1f
Ashutosh <ashutoshsolanki@yahoo.co.in>
2017-06-03 20:47:41 +0530

    Fixed student select problem

--- app_client/messages/selectRecipientsModal.controller.js
+++ app_client/messages/selectRecipientsModal.controller.js
@@ -6,30 +6,36 @@
   
   selectRecipientsModalCtrl.$inject = ['$modalInstance','handbookData','authentication','currentSelectedStudents'];  
   
+  
   function selectRecipientsModalCtrl($modalInstance,handbookData,authentication,currentSelectedStudents) {
     var vm = this;
     vm.students=currentSelectedStudents.students;
     
     //Mark all students as selected by default
-    
+    var changeSelectionState=function(state){
+          angular.forEach(vm.students,function(student){
+          student.selected=state;
+        })
+      }  
     
     var selectedStudentsList =[];
    
     
     vm.checkAll=function(){
 
-      if(!vm.selectedAll){
-        vm.selectedAll=true
-      }
-      else{
-        vm.selectedAll=false;
-      }
-      angular.forEach(vm.students,function(student){
-        student.selected=vm.selectedAll;
-      })
+      // if(!vm.selectedAll){
+      //   vm.selectedAll=true
+      // }
+      // else{
+      //   vm.selectedAll=false;
+      // }
+      changeSelectionState(vm.selectedAll)
 
     }
-    vm.checkAll();
+    
+
+    vm.selectedAll=true;
+    changeSelectionState(vm.selectedAll);
     vm.modal = {
         close : function (result) {
             $modalInstance.close(result);
