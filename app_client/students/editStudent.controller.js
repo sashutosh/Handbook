58f6e1c20512ae1ea8fa58a2d4b45989ae940d78
ashutoshsolanki <ashutoshsolanki@yahoo.co.in>
2017-06-17 13:32:58 +0530

    Date of birth formatting

--- app_client/students/editStudent.controller.js
+++ app_client/students/editStudent.controller.js
@@ -12,12 +12,30 @@
         vm.myFile={};
         vm.uploadCompleted=false;
         vm.student = handbookData.getSelectedStudent();
+        vm.StudentDOB = formatDOB(vm.student.StudentDOB)
         vm.loading=false;
         vm.pageHeader = {
           title: 'Edit Student'
         };
 
+        var formatDOB=function(birthDate){
+          var date = new Date(birthDate);
+          year = date.getFullYear();
+          month = date.getMonth()+1;
+          dt = date.getDate();
+
+          if (dt < 10) {
+            dt = '0' + dt;
+          }
+          if (month < 10) {
+            month = '0' + month;
+          }
+          var fomattedDate =dt+"/"+month+"/"+year;
+          return fomattedDate;
+        }
+
         vm.onSubmit=function(){
+          vm.student.StudentDOB =vm.StudentDOB;
           handbookData.updateStudent(vm.student)
             .success(function(result){
               alert("Record updated successfully");  
